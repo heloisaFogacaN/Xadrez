a97f2ea4625b59449848a17adac618cc355486ca
heloisaFogacaN <miguelbertoldi1@gmail.com>
2023-06-07 20:50:48 -0300

    Implementa possíveis movimentos cavalo

--- src/Cavalo.java
+++ src/Cavalo.java
@@ -4,6 +4,51 @@ public class Cavalo extends Peca{
 
     @Override
     public ArrayList<Posicao> possiveisMovimentos(Tabuleiro tabuleiro) {
-        return null;
+        ArrayList<Posicao> possiveisMovimentos = new ArrayList<>();
+        Posicao posicaoAtual= this.getPosicao();
+        int posicaoNoTabuleiro = tabuleiro.getPosicoes().indexOf(posicaoAtual);
+
+        if(posicaoNoTabuleiro % 8 == 0){
+            if(posicaoNoTabuleiro >8 && posicaoNoTabuleiro <48){
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-15));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-6));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+10));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+17));
+            }
+            if(posicaoNoTabuleiro == 0){
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+10));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+17));
+            } else if(posicaoNoTabuleiro == 56){
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-15));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-6));
+            } else if(posicaoNoTabuleiro == 8) {
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-6));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+10));
+                possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+17));
+            }
+        }
+        possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-17));
+        possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro-10));
+        possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+6));
+        possiveisMovimentos.add(tabuleiro.getPosicoes().get(posicaoNoTabuleiro+15));
+
+        for(Posicao posicao: tabuleiro.getPosicoes()){
+                  if(tabuleiro.getPosicoes().indexOf(posicao) == posicaoNoTabuleiro -17 ||
+                    tabuleiro.getPosicoes().indexOf(posicao) == posicaoNoTabuleiro -10 ||
+                    tabuleiro.getPosicoes().indexOf(posicao) == posicaoNoTabuleiro -6 ||
+                    tabuleiro.getPosicoes().indexOf(posicao) == posicaoNoTabuleiro +6 ||
+                    tabuleiro.getPosicoes().indexOf(posicao) == posicaoNoTabuleiro +10 ||
+                    tabuleiro.getPosicoes().indexOf(posicao) == posicaoNoTabuleiro +17){
+                if(posicaoNoTabuleiro % 8 == 0){
+                    if(posicaoNoTabuleiro < 48 && posicaoNoTabuleiro > 8){
+                        possiveisMovimentos.add(posicao);
+                    }
+                }
+            }
+        }
+
+
+
+        return possiveisMovimentos;
     }
 }
